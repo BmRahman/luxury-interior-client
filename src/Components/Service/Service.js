b310e6cb0fefe6e2ca18c6b49363c0b6c7554304
BmRahman <bmrahman23@gmail.com>
2022-11-08 16:58:11 -0800

    details card done

---
+++ src/Components/Service/Service.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import './Service.css';
+
+const Service = () => {
+
+    const serviceData = useLoaderData()
+    console.log(serviceData)
+    const {image, title, _id, details, price, rating} = serviceData;
+
+
+    return (
+        <div className='my-10 mb-10'>
+           <div className="card w-full lg:w-4/5 mx-auto shadow-xl">
+                <figure><img src={image} alt="Service" /></figure>
+                    <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    <div>
+                        {details}
+                    </div>
+                    <div className='flex justify-between mt-6 mb-5'>
+                        <div>
+                            <p>Price: ${price}</p>
+                        </div>
+                        <div>
+                            <p>Ratings: {rating}</p>
+                        </div>
+                    </div>
+    <               div className="card-actions justify-end">
+                    <Link to={`/services/${_id}`}><button className="btn view">View Details</button></Link>
+                </div>
+            </div>
+        </div> 
+        </div>
+    );
+};
+
+export default Service;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layout/Main';
 import Home from '../Components/Home/Home';
 import Services from '../Components/Services/Services';
+import Service from '../Components/Service/Service';
 
 export const router = createBrowserRouter([
     {
@@ -21,6 +22,11 @@ export const router = createBrowserRouter([
                 path: '/services',
                 loader: () => fetch('http://localhost:5000/services'),
                 element: <Services></Services>
+            },
+            {
+                path: '/services/:id',
+                loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`),
+                element: <Service></Service>
             }
         ]
     }
