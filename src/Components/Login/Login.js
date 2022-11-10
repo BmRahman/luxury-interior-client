95819e094c7498aea5eb103962d4f89cdbed0f06
BmRahman <bmrahman23@gmail.com>
2022-11-09 23:33:33 -0800

    google login done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider';
+import { FcGoogle } from 'react-icons/fc';
+
 
 const Login = () => {
-    const {userLogin} = useContext(AuthContext);
+    const {userLogin, googleLogin} = useContext(AuthContext);
     const location = useLocation()
     const navigate = useNavigate()
 
@@ -26,6 +28,17 @@ const Login = () => {
         .catch(err => console.error(err))
     }
 
+    const handleGoogleLogin = () => {
+      googleLogin()
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+        navigate(from, { replace: true })
+      })
+      .catch(err => console.error(err))
+    }
+
+
     return (
         <div className='my-10 lg:mb-20'>
             <h2 className='text-center text-3xl lg:text-5xl font-bold mb-10'>Sign Up Here</h2>
@@ -45,6 +58,7 @@ const Login = () => {
         </div>
         <div className="form-control mt-6 w-2/5 mx-auto">
           <button type='submit' className="btn signup">Login</button>
+          <button className='btn mt-5' onClick={handleGoogleLogin}> <FcGoogle className='text-xl mr-3'></FcGoogle> Login With Google</button>
         </div>
         <p className='mt-5 text-center'>New To Luxury Interior? <Link to='/register' className='underline underline-offset-4'>Sign Up Here</Link></p>
       </div>
